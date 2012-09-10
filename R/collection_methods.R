a2e87640242920d8c4075916a8061601d5184c1d
Karthik Ram <karthik.ram@gmail.com>
2012-09-10 13:15:42 -0700

    added scaffolding for S3 methods for collection class.

---
+++ R/collection_methods.R
@@ -0,0 +1,11 @@
+# Collection methods
+# Scaffolding for methods (print, plot, save, although save is technically not a generic method), for total impact collections
+
+#' @return \code{NULL}
+#'
+#' @export
+#' @rdname ti_collection
+#' @S3method plot ti_collection
+print.ti_collection <- function(collection_id) {
+
+}
