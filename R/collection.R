47ffa3229cdf24ccc970892b6369c5634a971473
Karthik Ram <karthik.ram@gmail.com>
2012-09-18 08:35:05 -0600

    Getting closer to making the collection functions work.

--- R/collection.R
+++ R/collection.R
@@ -65,6 +65,8 @@ data <- collection_metrics(collection_id = cid, as.csv = TRUE)
 #' @export
 #' @return collection id
 #' @examples \dontrun{
+#' doi <- c("10.1145/1327452.1327492", "10.1080/10556781003625177")	
+#' pmid <- c("19782018","17420288", "17593900", "17420288")
 #' create_collection(title="my collection", list_of_items) # list must be valid total impact IDs.
 #'}
 create_collection <- function(title = NULL, objects = NULL) {
@@ -72,9 +74,28 @@ create_collection <- function(title = NULL, objects = NULL) {
 		stop("Please specify a title for your collection", call.=FALSE)
 
 	if(is.null(objects) || class(objects)!=list)
-		stop("You need to specify a list of objects to curate into a collection", call.=FALSE)
+		stop("You need to specify a list of objects to curate into a collection. See ?create_collection for examples.", call.=FALSE)
 
+
+ 
 	object_list <- toJSON(objects)	
+	browser()
 	new_id <- postForm('http://api.total-impact.org/collection', args = object_list, style = "POST")  	
 
 } 
+
+# Tests
+
+pmid <- as.list(c("19782018","17420288", "17593900", "17420288"))
+doi <- as.list(doi <- c("10.1145/1327452.1327492", "10.1080/10556781003625177")
+	)
+x=list(pmid = pmid, doi =doi)
+ names(pmid) <- rep("pmid", length(pmid))
+ xx <- llply(x, function(z) {
+ 	browser()
+ 	})
+
+ 
+ names(doi) <- rep("doi", length(doi))
+ all <- c(pmid, doi)
+object_list <- toJSON(all, .withNames = TRUE)
