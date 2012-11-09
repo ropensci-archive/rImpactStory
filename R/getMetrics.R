47064ae81660c998799a52b925f56078d80351aa
Karthik Ram <karthik.ram@gmail.com>
2012-11-08 21:11:07 -0800

    fixed metrics

--- R/getMetrics.R
+++ R/getMetrics.R
@@ -1,20 +1,26 @@
 
 #' Returns the metrics for a valid Impact Story ID
 #'
-#' @param id = NA A Impact Story ID
+#' @param id  A Impact Story ID
+#  @param nspace Namespace for the identifier. Valid namespaces include doi, github.
 #' @export
-#' @seealso \code{ISid}
 #' @examples \dontrun{
-#' my_id <- ISid('10.1890/ES11-00339.1')	
-#' metrics(my_id)
+#' Get metrics on a github repo
+#' metrics('karthikram,rtools', 'github')
+#' Get metrics on an article from its DOI
+#' metrics('10.1038/171737a0')
 #'}
 #' @author Karthik Ram \email{karthik.ram@@gmail.com}
-metrics <- function(id = NA) {
-if(is.null(id))
-	stop('A Impact Story id was not specified', call.=FALSE)
+metrics <- function(id = NA, nspace = 'doi') {
+if(is.na(id))
+	stop("No id specified", call.=FALSE)
 
-metrics <- getURL(paste0('http://api.impactstory.org/item/', id))
- if(length(grep('404', metrics))>0 && grep('404', metrics)==1) {
+id2 <- ISid(id, nspace)	
+
+metrics <- getURL(paste0("http://api.impactstory.org/item/", id2))
+
+
+ if(length(grep('404 Not Found', metrics))>0 && grep('404 Not Found', metrics)==1) {
 	stop("No metrics found on supplied Impact Story ID. Supplied ID may not be valid", call.= FALSE)
 }
 
