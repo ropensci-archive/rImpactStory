ac1a9baf1a2f6390f2e9ad6919f63a824b4b0753
Karthik Ram <karthik.ram@gmail.com>
2012-11-09 15:53:56 -0800

    Updated metrics help and documentation

--- R/getMetrics.R
+++ R/getMetrics.R
@@ -2,15 +2,15 @@
 #' Returns the metrics for a valid Impact Story ID
 #'
 #' @param id  An Impact Story ID
-#' @param nspace Namespace for the identifier. Valid namespaces include doi, github (among others).
+#' @param nspace Namespace for the identifier. Valid namespaces include doi, github (among others). Read more about available metrics here: http://impactstory.org/faq
 #' @export
 #' @examples \dontrun{
 #' Get metrics on a github repo
-#' metrics('karthikram,rtools', 'github')
+#' metrics('karthikram,rtools', nspace ='github')
 #' Get metrics on an article from its DOI
 #' metrics('10.1038/171737a0')
 #' # metrics on a figshare document
-#' metrics('10.6084/m9.figshare.97222', 'figshare')
+#' metrics('10.6084/m9.figshare.97222', nspace ='figshare')
 #'}
 #' @author Karthik Ram \email{karthik.ram@@gmail.com}
 metrics <- function(id = NULL, nspace = 'doi') {
@@ -18,9 +18,7 @@ if(is.null(id))
 	stop("No id specified", call.=FALSE)
 
 id2 <- ISid(id, nspace)	
-
 metrics <- getURL(paste0("http://api.impactstory.org/item/", id2))
-
 
  if(length(grep('404 Not Found', metrics))>0 && grep('404 Not Found', metrics)==1) {
 	stop("No metrics found on supplied Impact Story ID. Supplied ID may not be valid", call.= FALSE)
